dcfa470f27b36d9973bf59ebf36f5fd8f42b2097
Eftay Khyrul Alam <eftay.khyrul.alam.baiust.09@gmail.com>
2023-11-09 15:15:09 +0600

    Admin panel part 2,3, and logout

---
+++ resources/views/admin/body/footer.blade.php
@@ -0,0 +1,14 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-6">
+                Product Management System © Eftay Khyrul Alam.
+            </div>
+            <div class="col-sm-6">
+                <div class="text-sm-end d-none d-sm-block">
+                    Crafted with <i class="mdi mdi-heart text-danger"></i> by Product Management System
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
