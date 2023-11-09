5dc2e3186d05366b8e6d2bf901129ac2208ea58b
Eftay Khyrul Alam <eftay.khyrul.alam.baiust.09@gmail.com>
2023-11-09 11:20:06 +0600

    admin panel setup part 1 complete

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('admin.index');
     //added 'auth' and 'verified' middleware
 })->middleware(['auth', 'verified'])->name('dashboard');
 
